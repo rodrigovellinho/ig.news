8f808a376add68b21970a195079580c53ae3ad88
unknown <rodrigo.kloeckner@gmail.com>
2022-04-15 12:20:31 -0300

    files adj

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
-import styles from "./home.module.scss";
+import styles from "../../styles/home.module.scss";
 import { GetStaticProps } from "next";
 import { stripe } from "../services/stripe";
 
